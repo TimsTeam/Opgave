a0249435a76bdcfbe3330e156127954b5c0dfa18
cvd2412 <chri798x@elev.techcollege.dk>
2018-01-08 13:57:32 +0100

    login

--- incl/header.php
+++ incl/header.php
@@ -39,7 +39,7 @@
                     <a class="nav-link" href="index.php">Forside</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="login.php">Side</a>
+                    <a class="nav-link" href="login.php">Login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Side</a>
