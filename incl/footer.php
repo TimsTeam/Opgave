8f6b71a26539b899ad3b780c49152cf7cdb4ddce
Izæt <izet0004@elev.techcollege.dk>
2018-01-09 12:24:07 +0100

    Social fix -.-

--- incl/footer.php
+++ incl/footer.php
@@ -1,55 +1,58 @@
-
 <hr>
 <footer class="container">
 
-        <div class="row" >
-            <div class="col-xl-6">
-                <p>Adresse:</p>
-                <p>Øster Uttrup vej 1, 9000 Aalborg</p>
-                <br>
-                <p>Tlf:</p>
-                <p>88888888</p>
-                <br>
-                <p>Mail:</p>
-                <p>footsteps@foot.dk</p>
-            </div>
-                <div class="col-xl-6">
-                    <p>Find os her</p>
-                    <a href=""><img src="assets/img/facebook_logo.svg" alt="Pis" style="width: 30px;"> </a>
-                    <a href=""><img src="assets/img/instagram_logo.svg" alt="Pis" style="width: 30px;"> </a>
-            </div>
+    <div class="row">
+        <div class="col-xl-6">
+            <p>Adresse:</p>
+            <p>Øster Uttrup vej 1, 9000 Aalborg</p>
+            <br>
+            <p>Tlf:</p>
+            <p>88888888</p>
+            <br>
+            <p>Mail:</p>
+            <p>footsteps@foot.dk</p>
         </div>
+        <div class="col-xl-6 social">
+            <a href="">
+                <img src="assets/img/facebook_logo.svg" alt="Pis" class="fb" style="width: 30px;"> </a>
+            <a href="">
+                <img src="assets/img/instagram_logo.svg" alt="Pis" class="ig" style="width: 30px;"> </a>
+        </div>
+    </div>
 </footer>
 
 
-    <!-- ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<!-- ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+    crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+    crossorigin="anonymous"></script>
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script>
-        (function() {
-            'use strict'
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script>
+    (function () {
+        'use strict'
 
-            if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-                var msViewportStyle = document.createElement('style')
-                msViewportStyle.appendChild(
-                    document.createTextNode(
-                        '@-ms-viewport{width:auto!important}'
-                    )
+        if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+            var msViewportStyle = document.createElement('style')
+            msViewportStyle.appendChild(
+                document.createTextNode(
+                    '@-ms-viewport{width:auto!important}'
                 )
-                document.head.appendChild(msViewportStyle)
-            }
-
-        }())
-    </script>
-    <script>
-        function myFunction(x) {
-            x.classList.toggle("change");
+            )
+            document.head.appendChild(msViewportStyle)
         }
-    </script>
+
+    }())
+</script>
+<script>
+    function myFunction(x) {
+        x.classList.toggle("change");
+    }
+</script>
 </body>
 
 </html>
