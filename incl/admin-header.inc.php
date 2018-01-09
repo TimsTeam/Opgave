b16e3d1f39ec2456a3bcd290712515f3b1bb6705
cvd2412 <chri798x@elev.techcollege.dk>
2018-01-09 09:59:15 +0100

    .

--- incl/admin-header.inc.php
+++ incl/admin-header.inc.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="theme-color" content="#FFFFFF">
     <meta name="description" content="A blog site!">
-    <meta name="author" content="Christian V. D.">
+    <meta name="author" content="Timsteam">
     <title><?php echo "$pageTitle" ?></title>
     <!-- Bootstrap stylesheet -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
@@ -26,9 +26,9 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     <script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
     <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <!-- PopperJS -->
+    <!-- PopperJS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-        <!-- Bootstrap script -->
+    <!-- Bootstrap script -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
   </head>
 
@@ -44,4 +44,4 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
   </header>
   
 
-  <body class="white">
+  <body>
