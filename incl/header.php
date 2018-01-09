c199d652d5c3598f6881027262e93c7e7799948f
cvd2412 <chri798x@elev.techcollege.dk>
2018-01-09 08:43:35 +0100

    .

--- incl/header.php
+++ incl/header.php
@@ -26,7 +26,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="index.php"><b>FOOT</b>steps</a>
+        <a class="navbar-brand" href="index.php"><b>FOOT</b>STEPS</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onclick="myFunction(this)">
             <div class="bar1"></div>
             <div class="bar2"></div>
