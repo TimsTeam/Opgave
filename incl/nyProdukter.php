dd3db0c3e8304497418f5dc0dbeeedee70ee64f0
kris783c <kristianholmsten@gmail.com>
2018-01-09 10:11:06 +0100

    Index update
    +findOs.php
    +ny linje på header.php
    +nyProdukter.php
    +indsat flere includes til index.php

---
+++ incl/nyProdukter.php
@@ -0,0 +1,31 @@
+<section class="container">
+  <center>
+    <h1>Nye Produkter</h1>
+    <div class="row justify-content-xl-center nyCard">
+      <div class="card col-xl-3" style="width: 20rem;">
+        <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+        <div class="card-block">
+          <h4 class="card-title">Card title</h4>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div class="card col-xl-3" style="width: 20rem;">
+        <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+        <div class="card-block">
+          <h4 class="card-title">Card title</h4>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      <div class="card col-xl-3" style="width: 20rem;">
+        <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+        <div class="card-block">
+          <h4 class="card-title">Card title</h4>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </center>
+</section>
