db2749dfe0cc27a30af29d4c2ae155fcf4f6003d
kris783c <kristianholmsten@gmail.com>
2018-01-10 14:20:46 +0100

    Filter Markup

--- incl/skoContent.php
+++ incl/skoContent.php
@@ -2,11 +2,12 @@
 
   <!-- Venstre side (filter) -->
   <div class="col-xl-2 skoBox">
-      <br />
-      <div class="skoBox2">
-        <span>Størrelse</span>
-      </div>
-      <center>
+    <br />
+    <!-- Størrelse -->
+    <div class="skoBox2">
+      <span>Størrelse</span>
+    </div>
+    <center>
       <div class="form-check-inline" class="strFix">
         <label class="form-check-label">
           <input class="form-check-input" type="checkbox" value="">
@@ -46,9 +47,93 @@
         </label>
       </div>
       <br />
-    </center>
+      <!-- Farve -->
 
-  </div>
+      <div class="skoBox2">
+        <span>Farver</span>
+      </div>
+      <div class="color-green colorbox">
+      </div>
+      <div class="color-red colorbox">
+      </div>
+      <div class="color-yellow colorbox">
+      </div>
+      <div class="color-white colorbox">
+      </div>
+      <div class="color-blue colorbox">
+      </div>
+      <div class="color-purple colorbox">
+      </div>
+      <div class="color-orange colorbox">
+      </div>
+      <div class="color-black colorbox">
+      </div>
+      <div class="color-teal colorbox">
+      </div>
+
+      <!-- Type -->
+      <div class="skoBox2">
+        <span>Type</span>
+      </div>
+
+      <div class="form-check">
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+    Sneakers
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Stiletter
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Sandaler
+        </label>
+      </div>
+      <br />
+      <!-- Mærker -->
+      <div class="skoBox2">
+        <span>Type</span>
+      </div>
+      <div class="form-check mrkFix">
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Nike
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Adidas
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Vans
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Puma
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Gucci
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Birkenstock
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          DAY
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          H2O
+        </label>
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" value="">
+          Ecco
+        </label>
+      </div>
+    </center>
   </div>
 
   <div class="col-xl-9">
