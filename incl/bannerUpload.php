6f3f18084e8eb1cdf4589b9d6b44be9ee656d082
Izæt <izet0004@elev.techcollege.dk>
2018-01-10 09:29:42 +0100

    Banner oversigt

--- incl/bannerUpload.php
+++ incl/bannerUpload.php
@@ -11,6 +11,34 @@ function validateData($connect, $data){
     return $data;
 }
 
+
+// Banner delete
+if(!empty(($_POST['deleteBanner']))){
+    // Prepare statement
+    $stmt = $dbConnect->prepare("DELETE from banner WHERE id = ?");
+    $stmtDeleteFile = $dbConnect->prepare("SELECT bannerName FROM banner where id = ?");
+    $id = $_POST['deleteBanner'];
+
+    // Bind param
+    $stmt->bind_param("i", $id);
+    $stmtDeleteFile->bind_param("i", $id);
+    if($stmtDeleteFile->execute()){
+        $stmtDeleteFile->store_result();
+        $stmtDeleteFile->bind_result($bannerName);
+        $stmtDeleteFile->fetch();
+        unlink($_SERVER['DOCUMENT_ROOT'] . "/assets/images/" . $bannerName);
+        if($stmt->execute()){
+        //Okay
+        } else {
+            // Failure
+        }
+    } else {
+        // Failure
+    }
+
+}
+
+// Banner upload
 if (isset($_POST["uploadImage"])) {
     $imgFile = $_FILES["imgFile"]; // Get file from submitted data
 
@@ -42,8 +70,8 @@ if (isset($_POST["uploadImage"])) {
                     }
 
                     
-                    $stmt->close(); // Close stmt connection
-                    $dbConnect->close(); // Close dbConnect connection
+                    // $stmt->close(); // Close stmt connection
+                    // $dbConnect->close(); // Close dbConnect connection
                 } else {
                     echo "File already exist!";
                 }
@@ -58,14 +86,46 @@ if (isset($_POST["uploadImage"])) {
         echo "Wrong image format";
     }   
 }
+
 ?>
 
-    <section class="container">
-        <form method="POST" action="" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="imgFile">Upload image</label>
-                <input type="file" name="imgFile" class="form-control-file" id="imgFile">
-                <button type="submit" name="uploadImage" class="btn btn-primary">Upload</button>
-            </div>
-        </form>
+    <section class="container bannerUpload">
+        <div class="row">
+            <form class="col" method="POST" action="" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="imgFile">Upload image</label>
+                    <input type="file" name="imgFile" class="form-control-file" id="imgFile">
+                    <button type="submit" name="uploadImage" class="btn btn-primary">Upload</button>
+                </div>
+            </form>
+            <form class="table-responsive col" method="POST" action="">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Thumbnail</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Banner overview
+                        $query2 = "SELECT * from banner ORDER BY id DESC";
+                        $result2 = mysqli_query($dbConnect, $query2);
+                        $result2Check = mysqli_num_rows($result2);
+                        if($result2Check > 0){
+                            while($row = mysqli_fetch_assoc($result2)){
+                                echo ('<tr>');
+                                echo ('<th scope="row">'. $row['id'] . '</th>');
+                                echo ('<td>' . $row['bannerName'] . '</td>');
+                                echo ('<td><img src="/assets/images/' . $row['bannerName'] . '"></td>');
+                                echo ('<td><button type="submit" name="deleteBanner" value="' . $row['id'] . '" class="btn btn-danger">Delete</button></td>');
+                                echo ('</tr>');
+                            }
+                        }
+                        
+                        ?>
+            </form>
+        </div>
     </section>
