5ed9d3cad723dd3504757727f8d31676872eb59a
kris783c <kristianholmsten@gmail.com>
2018-01-10 08:45:39 +0100

    Index Update
    +Mere mellemrum over Nye produkter

--- incl/nyProdukter.php
+++ incl/nyProdukter.php
@@ -2,7 +2,7 @@
   <center>
     <h1>Nye Produkter</h1>
     <div class="row">
-      <div class="card col-xl-3 mx-auto" style="width: 20rem;">
+      <div class="card col-xl-3 mx-auto np" style="width: 20rem;">
         <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
         <div class="card-block">
           <h4 class="card-title">Card title</h4>
@@ -10,7 +10,7 @@
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
-      <div class="card col-xl-3 mx-auto" style="width: 20rem;">
+      <div class="card col-xl-3 mx-auto np" style="width: 20rem;">
         <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
         <div class="card-block">
           <h4 class="card-title">Card title</h4>
@@ -18,12 +18,13 @@
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
-      <div class="card col-xl-3 mx-auto" style="width: 20rem;">
+      <div class="card col-xl-3 mx-auto np" style="width: 20rem;">
         <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
-        <div class="card-block">
+        <div class="card-block ">
           <h4 class="card-title">Card title</h4>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
+          <br><br>
         </div>
       </div>
     </div>
