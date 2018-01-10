c1f8f73f052d62793cc6937f0d5071a10cdbc131
KristianEllgaardJonsson <kristian.jonsson2@gmail.com>
2018-01-10 09:06:15 +0100

    vans

---
+++ blog.php
@@ -0,0 +1,62 @@
+<?php include $_SERVER["DOCUMENT_ROOT"]."/incl/header.php";?>
+
+<h1>Footsteps blog :</h1>
+<div class="container">
+<div class="row">
+    <div class="card col-xl-3 mx-auto" style="width: 20rem;">
+        <img class="card-img-top" src="assets/img/vans.jpg" alt="Card image cap">
+        <div class="card-block">
+            <h4 class="card-title">Nye vans på lager</h4>
+            <p class="card-text">Vi har fået nye vans på lager </p>
+            <a href="#" class="btn btn-primary">Læs mere</a>
+        </div>
+    </div>
+    <div class="card col-xl-3 mx-auto" style="width: 20rem;">
+        <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+        <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card col-xl-3 mx-auto" style="width: 20rem;">
+        <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+        <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+    <br>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="card col-xl-3 mx-auto np" style="width: 20rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-block">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-xl-3 mx-auto" style="width: 20rem;">
+            <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+            <div class="card-block">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-xl-3 mx-auto" style="width: 20rem;">
+            <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+            <div class="card-block">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include $_SERVER["DOCUMENT_ROOT"]."/incl/footer.php";?>
