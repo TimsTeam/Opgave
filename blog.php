ba1359f8876dbc9ad991c5dde1c611ed92b22750
KristianEllgaardJonsson <kristian.jonsson2@gmail.com>
2018-01-10 14:21:13 +0100

    blog

--- blog.php
+++ blog.php
@@ -12,7 +12,7 @@
         </div>
     </div>
     <div class="card col-xl-3 mx-auto" style="width: 20rem;">
-        <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+        <img class="card-img-top" src="assets/img/vans.jpg" alt="Card image cap">
         <div class="card-block">
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -20,7 +20,7 @@
         </div>
     </div>
     <div class="card col-xl-3 mx-auto" style="width: 20rem;">
-        <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+        <img class="card-img-top" src="assets/img/vans.jpg" alt="Card image cap">
         <div class="card-block">
             <h4 class="card-title">Card title</h4>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -33,7 +33,15 @@
 <div class="container">
     <div class="row">
         <div class="card col-xl-3 mx-auto np" style="width: 20rem;">
-            <img class="card-img-top" src="..." alt="Card image cap">
+            <img class="card-img-top" src="assets/img/vans.jpg" alt="Card image cap">
+            <div class="card-block">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-xl-3 mx-auto" style="...">
+            <img class="card-img-top" src="assets/img/vans.jpg" alt="Card image cap">
             <div class="card-block">
                 <h4 class="card-title">Card title</h4>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -41,15 +49,7 @@
             </div>
         </div>
         <div class="card col-xl-3 mx-auto" style="width: 20rem;">
-            <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
-            <div class="card-block">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card col-xl-3 mx-auto" style="width: 20rem;">
-            <img class="card-img-top" src="/yzy.jpg" alt="Card image cap">
+            <img class="card-img-top" src="assets/img/vans.jpg" alt="Card image cap">
             <div class="card-block">
                 <h4 class="card-title">Card title</h4>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
