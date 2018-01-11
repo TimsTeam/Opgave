29ee481de115f0b7fd5006eb13a527a0bd078ab2
amaliesteenberg <amaliesteenberg@10.161.15.196>
2018-01-11 12:01:54 +0100

    slettede <?php og ?>

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<?php $pagetitle = "g"; ?>
-<?php include $_SERVER["DOCUMENT_ROOT"]."/incl/header.php";?>
+<?php $pagetitle = "g";
 
-<?php include $_SERVER["DOCUMENT_ROOT"]."/incl/banner.php";?>
+include $_SERVER["DOCUMENT_ROOT"]."/incl/header.php";
 
-<?php include $_SERVER["DOCUMENT_ROOT"]."/incl/nyProdukter.php";?>
+include $_SERVER["DOCUMENT_ROOT"]."/incl/banner.php";
 
-<?php include $_SERVER["DOCUMENT_ROOT"]."/incl/findOs.php";?>
+include $_SERVER["DOCUMENT_ROOT"]."/incl/nyProdukter.php";
 
-<?php include $_SERVER["DOCUMENT_ROOT"]."/incl/footer.php";?>
+include $_SERVER["DOCUMENT_ROOT"]."/incl/findOs.php";
+
+include $_SERVER["DOCUMENT_ROOT"]."/incl/footer.php";?>
